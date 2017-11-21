3b410dc19fc2265b9e48887afcd052e80f779360
StijnDeBrauwer <db_stijn@hotmail.com>
2017-11-21 20:33:03 +0100

    Verplaats routing naar routing module

---
+++ webapps-tabletennisapp/src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes} from '@angular/router';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MaterializeModule } from 'angular2-materialize';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { NewsDataService } from '../news-data.service';
+import { NewsComponent } from '../news/news.component';
+import { NewsitemComponent } from '../newsitem/newsitem.component';
+import { PlayerComponent } from '../player/player.component';
+import { SeriesComponent } from '../series/series.component';
+import { BlogComponent } from '../blog/blog.component';
+import { PagenotfoundComponent } from '../pagenotfound/pagenotfound.component';
+import { LoginComponent } from '../login/login.component';
+
+const appRoutes: Routes = [
+    { path: 'news', component: NewsComponent },
+    { path: 'series', component: SeriesComponent },
+     {path: 'blog', component: BlogComponent},
+    {path: 'login', component: LoginComponent},
+    { path: '', redirectTo: 'news', pathMatch: 'full'},
+    { path: '**', component: PagenotfoundComponent}
+  ];
+
+  @NgModule({
+    imports: [
+      FormsModule,
+      ReactiveFormsModule,
+      MaterializeModule,
+      CommonModule,
+      RouterModule.forRoot(
+        appRoutes
+      ),
+      ChartsModule,
+    ],
+    declarations: [
+        NewsComponent,
+        NewsitemComponent,
+        PlayerComponent,
+        SeriesComponent,
+        BlogComponent,
+        PagenotfoundComponent,
+        LoginComponent
+    ],
+    exports: [
+      RouterModule
+    ],
+    providers: [NewsDataService]
+  })
+export class AppRoutingModule { }
--- webapps-tabletennisapp/src/app/app.module.ts
+++ webapps-tabletennisapp/src/app/app.module.ts
@@ -3,41 +3,26 @@ import { MaterializeModule } from 'angular2-materialize';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { NewsComponent } from './news/news.component';
+import { NewsDataService } from './news-data.service';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
-import { NewsitemComponent } from './newsitem/newsitem.component';
-import { PlayerComponent } from './player/player.component';
-import { ChartsModule } from 'ng2-charts/ng2-charts';
-import { NewsDataService } from './news-data.service';
-import { RouterModule, Routes} from '@angular/router';
-import { SeriesComponent } from './series/series.component';
-import { BlogComponent } from './blog/blog.component';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
-const appRoutes: Routes = [
-  { path: 'news', component: NewsComponent },
-  { path: 'series', component: SeriesComponent },
-  {path: 'blog', component: BlogComponent},
-  { path: '', redirectTo: 'news', pathMatch: 'full'},
-  { path: '**', component: PagenotfoundComponent}
-];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewsComponent,
     MenuComponent,
     FooterComponent,
-    NewsitemComponent,
-    PlayerComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes),
     MaterializeModule,
     HttpModule,
-    ChartsModule
+    ChartsModule,
+    AppRoutingModule
   ],
 
   providers: [NewsDataService],
